c8b4e52a5f813916dbb10635c8804612e78e5fa2
ernokanst <ernokanst@gmail.com>
2023-04-06 15:57:48 +0300

    Первый экран

--- Finatlon/ContentView.swift
+++ Finatlon/ContentView.swift
@@ -8,18 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var didLogin = false
+    
     var body: some View {
-        VStack {
-            Text("Финатлон")
-                .font(.title)
-            TextField("Логин", text: <#T##Binding<String>#>)
-        }
-        .padding()
+            return Group {
+                if didLogin {
+                    MainView()
+                } else {
+                    LoginView(didLogin: $didLogin)
+                }
+                
+            }
     }
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
+struct LoginView: View {
+    @State private var username: String = ""
+    @State private var password: String = ""
+    @Binding var didLogin: Bool
+    
+    var body: some View {
+        VStack(alignment: .center, spacing: 20.0) {
+            Text("Финатлон").font(.title)
+            TextField("Имя пользователя (email)", text: $username).textInputAutocapitalization(.never).disableAutocorrection(true)
+            SecureField(text: $password, prompt: Text("Пароль")) {Text("пароль")}.textInputAutocapitalization(.never).disableAutocorrection(true)
+            Button("Войти", action: {self.didLogin = true}).buttonStyle(.borderedProminent)
+            Button("Зарегистрироваться", action: {self.didLogin = true})
+        }.textFieldStyle(.roundedBorder)
+            .padding()
     }
 }
+
+struct MainView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+/*struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView(didLogin: $didLogin)
+    }
+}
+*/
